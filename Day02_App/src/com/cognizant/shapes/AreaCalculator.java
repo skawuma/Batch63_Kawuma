5d2ca5cd33759227e43c4d8323d6f127962455a0
skawuma <82976186+skawuma@users.noreply.github.com>
2022-09-20 23:42:03 -0400

    Solution to Hands on Exercise

---
+++ Day02_App/src/com/cognizant/shapes/AreaCalculator.java
@@ -0,0 +1,17 @@
+package com.cognizant.shapes;
+
+/**
+ * @author samuelkawuma
+ *
+ */
+
+public class AreaCalculator {
+	  public static void main(String args[]){
+		  
+		 Rectangle r1 = new Rectangle();
+		 r1.calculateArea();
+		  
+	  }
+
+}
+
---
+++ Day02_App/src/com/cognizant/shapes/Rectangle.java
@@ -0,0 +1,15 @@
+package com.cognizant.shapes;
+
+/**
+ * @author samuelkawuma
+ *
+ */
+
+public class Rectangle {
+	void calculateArea() {
+		
+		System.out.println("The Area of the rectangle is calculated Using the formula lenght* breadth");
+	}
+
+}
+
