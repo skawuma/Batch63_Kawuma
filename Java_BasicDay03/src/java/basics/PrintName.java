98044be136192de805e767a282813d25aa64b544
skawuma <82976186+skawuma@users.noreply.github.com>
2022-09-21 10:06:30 -0400

    First Commit

---
+++ Java_BasicDay03/src/java/basics/PrintName.java
@@ -0,0 +1,24 @@
+package java.basics;
+
+/**
+ * @author samuelkawuma
+ *
+ */
+
+public class PrintName {
+	String name;
+	public String printName(String name) {
+		return name;
+		
+	
+	}
+	public static void main(String[] args) {
+		
+		PrintName n = new PrintName();
+		
+	n.printName("Samuel Kawuma");
+		
+	}
+
+}
+
