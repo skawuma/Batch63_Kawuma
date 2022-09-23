94df483321f667c4706685476592dd163c919f24
skawuma <82976186+skawuma@users.noreply.github.com>
2022-09-22 22:27:11 -0400

    Code is in Brute force; yet to work on more efficient Algorithm.

---
+++ Java_BasicDay03/src/com/cogent/arrays/SortedArrayAscend.java
@@ -0,0 +1,58 @@
+package com.cogent.arrays;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+/**
+ * @author samuelkawuma
+ *Sep 22, 2022
+ */
+
+public class SortedArrayAscend {
+	static void sortArray(int arr[])
+	{
+		int temp=0;
+		for(int i = 0; i< arr.length; i++) {
+			for(int j =i+1; j< arr.length; j++)
+				if(arr[j] < arr[i]) {
+					temp =arr[i];
+					arr[i]= arr[j];
+					arr[j] =temp;
+				}	
+		}
+
+	}
+	public static void main(String[] args) {
+		char answer = 'y';
+
+		while (Character.toUpperCase(answer) == 'Y') {
+			try {
+				Scanner s = new Scanner(System.in);
+				System.out.println("Please enter The Size of your preffered ArraySize");
+				//creates an array in the memory of length n
+				int n = s.nextInt();
+				System.out.println("enter random numbers of array size " + n);
+				int[] a = new int[n];
+
+				for (int i = 0; i < a.length; i++) {
+					//reading the number of elements from the that we want to enter
+					a[i] = s.nextInt();
+				}
+				System.out.println( "The Original Array is "+ Arrays.toString(a));
+				sortArray(a);
+				System.out.println("The sorted Array is "+ Arrays.toString(a) );
+				System.out.print("\n Do you want to continue (y or n)?");
+				Scanner keyboard = new Scanner(System.in);
+				answer = keyboard.next().charAt(0);
+
+			}catch (InputMismatchException e) {
+
+				System.out.println("Oops that was an invalid input!!! TRY AGAIN");
+
+			}
+		}
+	}
+}
+
+
+
+
