aa6b54e09af9a9288548f99d25a491af5a77b101
skawuma <s_kawuma@salemstate.edu>
2023-01-24 14:51:07 -0500

    update

---
+++ ClassActitvity(Novemeber21)/src/LargestInArray.java
@@ -0,0 +1,28 @@
+
+public class LargestInArray {
+	
+	
+	// Method to find maximum in arr[] 
+	static int largest(int []arr) 
+	{ 
+		int i; 
+		
+		// Initialize maximum element 
+		int max = arr[0]; 
+		
+		// Traverse array elements from second and 
+		// compare every element with current max 
+		for (i = 1; i < arr.length; i++) 
+			if (arr[i] > max) 
+				max = arr[i]; 
+		
+		return max; 
+	} 
+	
+	public static void main(String[] args) 
+	{ 
+	    int arr[] = {10, 324, 45, 90, 9808}; 
+		System.out.println("Largest in given array is " + largest(arr)); 
+		} 
+
+}
